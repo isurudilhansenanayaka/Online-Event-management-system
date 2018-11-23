b1bf55a071515c69ac9680655cf82f1a501c6038
Sajanauk <43530609+Sajanauk@users.noreply.github.com>
2018-11-24 02:06:54 +0530

    Add files via upload

---
+++ user_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php include 'header.php'; ?>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/homeStyle.css">
+    <link rel="stylesheet" href="/css/card.css">
+    <meta charset="utf-8">
+    <title></title>
+
+
+  </head>
+  <body>
+    <br>
+    <center>
+		<p><h1>I'm user account</h1><p>
+		<a type="submit" href="addv_add.php" >
+		<button type="button" > Make Advertisment</button></a>
+      
+    </center>
+  <br>
